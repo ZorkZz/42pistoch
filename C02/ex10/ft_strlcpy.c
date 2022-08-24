746c5f9e6d125b5f51b41a87ecfb9592d48cd470
ZorkZz <111425198+ZorkZz@users.noreply.github.com>
2022-08-24 15:29:10 +0200

    Add files via upload

--- C02/ex10/ft_strlcpy.c
+++ C02/ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: astachni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/08 09:47:27 by astachni          #+#    #+#             */
-/*   Updated: 2022/08/16 21:18:23 by astachni         ###   ########lyon.fr   */
+/*   Updated: 2022/08/23 11:14:49 by astachni         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
@@ -14,11 +14,19 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	unsigned int	i;
 
 	i = 0;
-	while (src[i] && i < size - 1)
+	if (!dest || !src)
+		return (0);
+	if (size > 0)
 	{
-		dest[i] = src[i];
-		i++;
+		while (src[i] && size != 0)
+		{
+			dest[i] = src[i];
+			i++;
+			size--;
+		}
+		dest[i] = '\0';
 	}
-	dest[i] == '\0';
+	while (src[i])
+		i++;
 	return (i);
 }
